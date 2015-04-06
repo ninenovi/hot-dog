335ebe149b22092e5f156c315f93be39f7353b53
Nine <Nine@Nine-PC>
2015-04-07 04:23:49 +0700

    TP RPL MOD8

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Nine
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, saya nine. nim saya 6306130040. Saya sangat menyukai praktikum rpl modul 8 ini)");
+    }
+}
